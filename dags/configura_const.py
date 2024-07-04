9ae7d0a72859e6322528512035e8d48398523370
Long Nguyen <zedsc03@gmail.com>
2024-07-04 15:11:42 +0700

    Update configuration constants

--- dags/configura_const.py
+++ dags/configura_const.py
@@ -6,12 +6,17 @@ from typing import Any
 S3_FOLDER_LOG = "logs"
 S3_FOLDER_INPUT = "input"
 S3_FOLDER_OUTPUT = "output"
+S3_FOLDER_SCRIPT = "scripts"
 S3_BUCKET_NAME = "spark-tf-processing-s3"
 S3_FILENAME = "Orders.csv"
 TEMP_FILE_PATH = "/opt/airflow/data/Orders.csv"
+
 LOG_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_LOG}/"
 S3_KEY = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_INPUT}/{S3_FILENAME}"
 FILE_PATH = Path(__file__).joinpath("..", "..", "data", "Orders.csv").resolve()
+
+EC2_SUBNET_ID = ""
+EC2_KEY_NAME = ""
 
 SECURITY_CONFIGURATION: dict[str, dict] = {
     "AuthorizationConfiguration": {
@@ -25,17 +30,13 @@ SECURITY_CONFIGURATION: dict[str, dict] = {
     },
 }
 
-SPARK_STEPS: list[dict[str, Any]] = [
+SPARK_STEPS_EXTRACTION: list[dict[str, Any]] = [
     {
-        "Name": "calculate_pi",
-        "ActionOnFailure": "CONTINUE",
+        "Name": "Extract Orders Data",
+        "ActionOnFailure": "CANCEL_AND_WAIT",
         "HadoopJarStep": {
-            "Jar": "command-runner.jar",
-            "Args": [
-                "/usr/lib/spark/bin/run-example", 
-                "SparkPi", 
-                "10"
-            ],
+            "Jar": "s3://ap-southeast-2.elasticmapreduce/libs/script-runner/script-runner.jar",
+            "Args": [f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_SCRIPT}/ingest.sh"],
         },
     }
 ]
@@ -68,12 +69,12 @@ JOB_FLOW_OVERRIDES: dict[str, Any] = {
             }
         ],
         
-        "Ec2SubnetId": "",
-        "Ec2KeyName": "",
+        "Ec2SubnetId": EC2_SUBNET_ID,
+        "Ec2KeyName": EC2_KEY_NAME,
         "KeepJobFlowAliveWhenNoSteps": True,
         "TerminationProtected": False,
     },
-    "Steps": SPARK_STEPS,
+    "Steps": SPARK_STEPS_EXTRACTION,
     "JobFlowRole": "EMR_EC2_DefaultRole",
     "ServiceRole": "EMR_DefaultRole",
 }
