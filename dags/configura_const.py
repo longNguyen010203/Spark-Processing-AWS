3a0a8e4e57e6dcf9a033d70a9c1895ef2bb54d16
Long Nguyen <zedsc03@gmail.com>
2024-07-07 18:08:01 +0700

    Change and add configura const

--- dags/configura_const.py
+++ dags/configura_const.py
@@ -2,31 +2,43 @@ from pathlib import Path
 from typing import Any
 
 
+AWS_CONN_ID = "aws_conn"
 
+S3_BUCKET_NAME = "amazon-books-bucket"
+S3_FOLDER_ROOT = "amazon-books-reviews"
 S3_FOLDER_LOG = "logs"
-S3_FOLDER_INPUT = "input"
-S3_FOLDER_OUTPUT = "output"
 S3_FOLDER_SCRIPT = "scripts"
-S3_REGION_NAME = "ap-southeast-2"
-S3_BUCKET_NAME = "spark-tf-processing-s3"
-S3_FILENAME = "Orders.csv"
-S3_BUCKET_DELIMITER = "/"
-TRANSFORM_FILE_NAME = ""
-TEMP_FILE_PATH = "/opt/airflow/data/Orders.csv"
+SHELL_FILENAME = "ingest.sh"
+SHELL_FILE_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/{SHELL_FILENAME}"
+S3_KEY_SHELL = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{SHELL_FILENAME}"
+LOG_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_LOG}/"
 
-TRANSFORM_FILE_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_SCRIPT}/{TRANSFORM_FILE_NAME}"
-LOG_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_LOG}/"
-S3_KEY = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_INPUT}/{S3_FILENAME}"
+TRANSFORM_BOOK_REVIEW = ".py"
+TRANSFORM_BOOK_DETAIL = ".py"
+TRANSFORM_BOOK_REVIEW_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/amazon_book_transform/{TRANSFORM_BOOK_REVIEW}"
+TRANSFORM_BOOK_DETAIL_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/amazon_book_transform/{TRANSFORM_BOOK_DETAIL}"
+TRANSFORM_RIVIEW_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{TRANSFORM_BOOK_REVIEW}"
+TRANSFORM_DETAIL_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{TRANSFORM_BOOK_REVIEW}"
+
+COMPUTE_BOOKS_NAME = "book_compute.py"
+COMPUTE_BOOKS_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/amazon_book_compute/{COMPUTE_BOOKS_NAME}"
+COMPUTE_BOOKS_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{COMPUTE_BOOKS_NAME}"
+
+DATA = ""
+VPC_NAME = ""
+
+
+S3_REGION_NAME = "ap-southeast-2"
+
+
 FILE_PATH = Path(__file__).joinpath("..", "..", "data", "Orders.csv").resolve()
 
-AWS_CONN_ID = "s3_bucket_connection"
 REDSHIFT_CONN_ID = ""
 
-EC2_SUBNET_ID = ""
-EC2_KEY_NAME = ""
 REDSHIFT_CLUSTER_IDENTIFIER = ""
 REDSHIFT_SCHEMA = "PUBLIC"
 REDSHIFT_TABLE = ""
+
 
 SECURITY_CONFIGURATION: dict[str, dict] = {
     "AuthorizationConfiguration": {
@@ -46,32 +58,59 @@ SPARK_STEPS_EXTRACTION: list[dict[str, Any]] = [
         "ActionOnFailure": "CANCEL_AND_WAIT",
         "HadoopJarStep": {
             "Jar": f"s3://{S3_REGION_NAME}.elasticmapreduce/libs/script-runner/script-runner.jar",
-            "Args": [f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_SCRIPT}/ingest.sh"],
+            "Args": [f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{SHELL_FILENAME}"],
         },
     }
 ]
 
-SPARK_STEPS_TRANSFORMATION: list[dict[str, Any]] = [
+SPARK_TRANSFORMATION_REVIEW: list[dict[str, Any]] = [
     {
-        "Name": "Transform Orders Data",
+        "Name": "Transform Reviews Data",
         "ActionOnFailure": "CANCEL_AND_WAIT",
         "HadoopJarStep": {
             "Jar": "command-runner.jar",
             "Args": [
                 "spark-submit",
-                f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_SCRIPT}/{TRANSFORM_FILE_NAME}"
+                TRANSFORM_RIVIEW_URI
+            ],
+        },
+    }
+]
+
+SPARK_TRANSFORMATION_DETAIL: list[dict[str, Any]] = [
+    {
+        "Name": "Transform Details Data",
+        "ActionOnFailure": "CANCEL_AND_WAIT",
+        "HadoopJarStep": {
+            "Jar": "command-runner.jar",
+            "Args": [
+                "spark-submit",
+                TRANSFORM_DETAIL_URI
+            ],
+        },
+    }
+]
+
+SPARK_STEPS_COMPUTATION: list[dict[str, Any]] = [
+    {
+        "Name": "Computing books Data",
+        "ActionOnFailure": "CANCEL_AND_WAIT",
+        "HadoopJarStep": {
+            "Jar": "command-runner.jar",
+            "Args": [
+                "spark-submit",
+                COMPUTE_BOOKS_URI
             ],
         },
     }
 ]
 
 JOB_FLOW_OVERRIDES: dict[str, Any] = {
-    "Name": "Spark-tf-processing-emr",
+    "Name": "amazon-books-processing-emr",
     "ReleaseLabel": "emr-6.13.0",
     "Applications": [
         {"Name": "Spark"}, 
         {"Name": "JupyterEnterpriseGateway"},
-        {"Name": "Zeppelin"}
     ],
     "LogUri": LOG_URI,
     "VisibleToAllUsers": False,
@@ -89,16 +128,15 @@ JOB_FLOW_OVERRIDES: dict[str, Any] = {
                 "Market": "ON_DEMAND",
                 "InstanceRole": "CORE",
                 "InstanceType": "m5.xlarge",
-                "InstanceCount": 3,
+                "InstanceCount": 2,
             }
         ],
         
-        "Ec2SubnetId": EC2_SUBNET_ID,
-        "Ec2KeyName": EC2_KEY_NAME,
+        "Ec2SubnetId": "subnet-01e07eab1b91cc469",
+        "Ec2KeyName": "amazon-books-key",
         "KeepJobFlowAliveWhenNoSteps": True,
         "TerminationProtected": False,
     },
-    "Steps": SPARK_STEPS_EXTRACTION,
     "JobFlowRole": "EMR_EC2_DefaultRole",
     "ServiceRole": "EMR_DefaultRole",
 }
