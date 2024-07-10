7c5dad27269c181b6c83e0919880fe6135e55c1c
Long Nguyen <zedsc03@gmail.com>
2024-07-10 14:30:11 +0700

    Update configure const file

--- dags/configura_const.py
+++ dags/configura_const.py
@@ -4,8 +4,8 @@ from typing import Any
 
 AWS_CONN_ID = "aws_conn"
 
-S3_BUCKET_NAME = "amazon-books-bucket"
-S3_FOLDER_ROOT = "amazon-books-reviews"
+S3_BUCKET_NAME = "yellow-tripdata-nyc"
+S3_FOLDER_ROOT = "2024-01"
 S3_FOLDER_LOG = "logs"
 S3_FOLDER_SCRIPT = "scripts"
 S3_FOLDER_DATA = "data"
@@ -14,29 +14,35 @@ SHELL_FILE_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/{SHELL_FILENAME}"
 S3_KEY_SHELL = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{SHELL_FILENAME}"
 LOG_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_LOG}/"
 
-TRANSFORM_BOOK_REVIEW = ".py"
-TRANSFORM_BOOK_DETAIL = ".py"
-TRANSFORM_BOOK_REVIEW_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/amazon_book_transform/{TRANSFORM_BOOK_REVIEW}"
-TRANSFORM_BOOK_DETAIL_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/amazon_book_transform/{TRANSFORM_BOOK_DETAIL}"
-TRANSFORM_RIVIEW_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{TRANSFORM_BOOK_REVIEW}"
-TRANSFORM_DETAIL_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{TRANSFORM_BOOK_REVIEW}"
+# # s3://nyc-yellow-tripdata/2024-01/data/raw/yellow_tripdata_2024-01.parquet
+# RAW_FILE_NAME = "yellow_tripdata_2024-01.parquet"
+# RAW_FILE_PATH = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_DATA}/raw/{RAW_FILE_NAME}"
 
-BOOK_REVIEW_NAME = "Books_rating.csv"
-BOOK_DETAIL_NAME = "books_data.csv"
-BOOK_REVIEW_SOURCE = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_DATA}/raw/{BOOK_REVIEW_NAME}"
-BOOK_DETAIL_SOURCE = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_DATA}/raw/{BOOK_DETAIL_NAME}"
-BOOK_REVIEW_OUTPUT = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_DATA}/transform/{BOOK_REVIEW_NAME}"
-BOOK_DETAIL_OUTPUT = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_DATA}/transform/{BOOK_DETAIL_NAME}"
 
-COMPUTE_BOOKS_NAME = "book_compute.py"
-COMPUTE_BOOKS_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/amazon_book_compute/{COMPUTE_BOOKS_NAME}"
-COMPUTE_BOOKS_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{COMPUTE_BOOKS_NAME}"
+TRANSFORM_FILE_NAME = "yellow_tripdata_transform.py"
+TRANSFORM_FILE_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/nyc_tripdata_transform/{TRANSFORM_FILE_NAME}"
+TRANSFORM_FILE_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{TRANSFORM_FILE_NAME}"
+
+
+DATA_FILE_NAME = "yellow_tripdata_2024-01.parquet"
+BOOK_REVIEW_SOURCE = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_DATA}/raw/{DATA_FILE_NAME}"
+TRANSFORM_DATA_OUTPUT = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_DATA}/transform/{DATA_FILE_NAME}"
+
+
+COMPUTE_FILE_NAME = "yellow_tripdata_compute.py"
+COMPUTE_FILE_PATH = f"/opt/airflow/{S3_FOLDER_SCRIPT}/nyc_tripdata_compute/{COMPUTE_FILE_NAME}"
+COMPUTE_FILE_URI = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_SCRIPT}/{COMPUTE_FILE_NAME}"
+
+
+COMPUTE_DATA_OUTPUT = f"s3://{S3_BUCKET_NAME}/{S3_FOLDER_ROOT}/{S3_FOLDER_DATA}/compute/{DATA_FILE_NAME}"
+
+
 
 DATA = ""
 VPC_NAME = ""
 
 
-S3_REGION_NAME = "ap-southeast-2"
+S3_REGION_NAME = "eu-west-1"
 
 
 FILE_PATH = Path(__file__).joinpath("..", "..", "data", "Orders.csv").resolve()
@@ -46,6 +52,43 @@ REDSHIFT_CONN_ID = ""
 REDSHIFT_CLUSTER_IDENTIFIER = ""
 REDSHIFT_SCHEMA = "PUBLIC"
 REDSHIFT_TABLE = ""
+
+
+JOB_FLOW_OVERRIDES: dict[str, Any] = {
+    "Name": "nyc-tripdata-emr-cluster",
+    "ReleaseLabel": "emr-6.13.0",
+    "Applications": [
+        {"Name": "Spark"}, 
+        {"Name": "JupyterEnterpriseGateway"},
+    ],
+    "LogUri": LOG_URI,
+    "VisibleToAllUsers": False,
+    "Instances": {
+        "InstanceGroups": [
+            {
+                "Name": "Master node",
+                "Market": "ON_DEMAND",
+                "InstanceRole": "MASTER",
+                "InstanceType": "m5.xlarge",
+                "InstanceCount": 1,
+            },
+            {
+                "Name": "Master node",
+                "Market": "ON_DEMAND",
+                "InstanceRole": "CORE",
+                "InstanceType": "m5.xlarge",
+                "InstanceCount": 3,
+            }
+        ],
+        
+        "Ec2SubnetId": "subnet-03327f94e28f3f1e3",
+        "Ec2KeyName": "nyc-tripdata-emr-key",
+        "KeepJobFlowAliveWhenNoSteps": True,
+        "TerminationProtected": False,
+    },
+    "JobFlowRole": "EMR_EC2_DefaultRole",
+    "ServiceRole": "EMR_DefaultRole",
+}
 
 
 SECURITY_CONFIGURATION: dict[str, dict] = {
@@ -71,33 +114,20 @@ SPARK_STEPS_EXTRACTION: list[dict[str, Any]] = [
     }
 ]
 
-SPARK_TRANSFORMATION_REVIEW: list[dict[str, Any]] = [
+SPARK_TRANSFORMATION: list[dict[str, Any]] = [
     {
-        "Name": "Transform Reviews Data",
+        "Name": "Transform Yellow Tripdata Data",
         "ActionOnFailure": "CANCEL_AND_WAIT",
         "HadoopJarStep": {
             "Jar": "command-runner.jar",
             "Args": [
                 "spark-submit",
-                TRANSFORM_RIVIEW_URI
+                TRANSFORM_DATA_OUTPUT
             ],
         },
     }
 ]
 
-SPARK_TRANSFORMATION_DETAIL: list[dict[str, Any]] = [
-    {
-        "Name": "Transform Details Data",
-        "ActionOnFailure": "CANCEL_AND_WAIT",
-        "HadoopJarStep": {
-            "Jar": "command-runner.jar",
-            "Args": [
-                "spark-submit",
-                TRANSFORM_DETAIL_URI
-            ],
-        },
-    }
-]
 
 SPARK_STEPS_COMPUTATION: list[dict[str, Any]] = [
     {
@@ -107,44 +137,8 @@ SPARK_STEPS_COMPUTATION: list[dict[str, Any]] = [
             "Jar": "command-runner.jar",
             "Args": [
                 "spark-submit",
-                COMPUTE_BOOKS_URI
+                COMPUTE_DATA_OUTPUT
             ],
         },
     }
 ]
-
-JOB_FLOW_OVERRIDES: dict[str, Any] = {
-    "Name": "amazon-books-processing-emr",
-    "ReleaseLabel": "emr-6.13.0",
-    "Applications": [
-        {"Name": "Spark"}, 
-        {"Name": "JupyterEnterpriseGateway"},
-    ],
-    "LogUri": LOG_URI,
-    "VisibleToAllUsers": False,
-    "Instances": {
-        "InstanceGroups": [
-            {
-                "Name": "Master node",
-                "Market": "ON_DEMAND",
-                "InstanceRole": "MASTER",
-                "InstanceType": "m5.xlarge",
-                "InstanceCount": 1,
-            },
-            {
-                "Name": "Master node",
-                "Market": "ON_DEMAND",
-                "InstanceRole": "CORE",
-                "InstanceType": "m5.xlarge",
-                "InstanceCount": 2,
-            }
-        ],
-        
-        "Ec2SubnetId": "subnet-01e07eab1b91cc469",
-        "Ec2KeyName": "amazon-books-key",
-        "KeepJobFlowAliveWhenNoSteps": True,
-        "TerminationProtected": False,
-    },
-    "JobFlowRole": "EMR_EC2_DefaultRole",
-    "ServiceRole": "EMR_DefaultRole",
-}
